292506774507775100eaf206068d6f261079c5cb
Luc Libralesso <libralesso.l@gmail.com>
2021-08-29 20:44:01 +0200

    update dsatur implementation with priority queue (small gain in performance)

--- src/search/greedy_dsatur.rs
+++ src/search/greedy_dsatur.rs
@@ -1,9 +1,30 @@
-use std::cmp::max;
+use std::cmp::{Ordering, max, Ord};
 use std::rc::Rc;
 
+use priority_queue::PriorityQueue;
 use bit_set::BitSet;
 
-use crate::color::{ColoringInstance, Solution};
+use crate::color::{ColoringInstance, Solution, VertexId};
+
+#[derive(PartialEq, Eq)]
+struct DSatInfo {
+    dsat: usize,
+    degree: usize
+}
+
+impl Ord for DSatInfo {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.dsat.cmp(&other.dsat)
+            .then_with(|| self.degree.cmp(&other.degree))
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for DSatInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 /** implements a greedy DSATUR algorithm. This algorithm should be able to handle large scale
 instances (up to 1M nodes and 1T edges).
@@ -18,31 +39,34 @@ parameters:
 */
 pub fn greedy_dsatur(inst:Rc<dyn ColoringInstance>, show_completion:bool) -> Solution {
     let n:usize = inst.nb_vertices();
+    let mut remaining_vertices:PriorityQueue<VertexId, DSatInfo> = PriorityQueue::new();
+    for i in 0..n {
+        remaining_vertices.push(i, DSatInfo { dsat:0, degree:inst.degree(i)});
+    }
     let mut colors:Vec<Option<usize>> = vec![None ; n]; // colors[v] -> color assigned to vertex v
     let mut adj_colors:Vec<BitSet> = vec![BitSet::default() ; n]; // adj_colors[n] -> colors n sees
-    let mut nb_adj_colors:Vec<usize> = vec![0 ; n]; // nb_adj_colors[n] -> number of colors n sees.
     let mut nb_colored:usize = 0;
     let mut last_color:usize = 0;
-    while nb_colored < n {
+    loop {
         if show_completion && nb_colored % 1000 == 0 { println!("colored {} / {}...", nb_colored, n); }
-        // find uncolored segment with largest saturation degree, breaking ties by length
-        let current_vertex = (0..n)
-            .filter(|v| colors[*v] == None)
-            .max_by(|a, b| {
-                nb_adj_colors[*a].cmp(&nb_adj_colors[*b]).then_with(|| inst.degree(*a).cmp(&inst.degree(*b)))
-            }).unwrap();
-        // assign it its color
+        // get current vertex
+        let current_vertex = match remaining_vertices.pop() {
+            None => break,
+            Some(v) => v.0
+        };
+        // assign it a color
         let mut color:usize = 0;
         while adj_colors[current_vertex].contains(color) { color += 1; }
         colors[current_vertex] = Some(color);
         nb_colored += 1;
         last_color = max(last_color, color); // update nb colors
-        // check all its unasigned conflicting segments, and update their saturation information
+        // update saturation degree information
         for conflict_vertex in inst.neighbors(current_vertex).iter()
         .filter(|conflict_vertex| colors[**conflict_vertex] == None) {
             if !adj_colors[*conflict_vertex].contains(color) { // update adj_colors & nb_adj_colors
-                nb_adj_colors[*conflict_vertex] += 1;
+                // nb_adj_colors[*conflict_vertex] += 1;
                 adj_colors[*conflict_vertex].insert(color);
+                remaining_vertices.change_priority_by(conflict_vertex, |p| {p.dsat += 1;} )
             }
         }
     }
